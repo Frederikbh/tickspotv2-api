781aac1be976d8d95afdf126b320847ab107b62b
Matthew Rosenberg <matt@dynamicwebsolutions.com>
2018-12-07 15:59:06 -0500

    added initial tests of main class

--- test/common.js
+++ test/common.js
@@ -1,3 +1,15 @@
 'use strict';
-global.chai     = require( 'chai' );
-global.sinon    = require( 'sinon' );
+global.chai   = require( 'chai' );
+global.sinon  = require( 'sinon' );
+global.expect = global.chai.expect;
+const Service = require( '../lib/service' ); 
+
+sinon.stub(Service, 'makeRequest')
+.callsFake((user, auth, url, cb, options = {}, headers = {}) => {
+
+	const promise = new Promise((resolve, reject) => {
+		return resolve([undefined, [user, auth, url, cb, options, headers]]);
+	});
+
+	return Service.handleCallback(promise, cb);
+});
